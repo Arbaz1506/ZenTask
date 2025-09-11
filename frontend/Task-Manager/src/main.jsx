524a062eae84047534d4cc0e0d99765e63c8cf49
Arbaz Salam <arbazsalam15@gmail.com>
2025-09-11 15:59:22 +0530

    Main Error Solved

--- frontend/Task-Manager/src/main.jsx
+++ frontend/Task-Manager/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'  // <-- import this
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <HashRouter>  {/* <-- wrap App */}
+      <App />
+    </HashRouter>
   </StrictMode>,
 )
