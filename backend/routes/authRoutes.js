a05b748c9d2e67d3fcc0b9652c103ef74f2a3777
Arbaz Salam <arbazsalam15@gmail.com>
2025-09-13 16:26:29 +0530

    Auth Corrected

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,10 @@
 const express = require("express");
-const { registerUser, loginUser, getUserProfile, updateUserProfile } = require("../controllers/authController");
+const {
+  registerUser,
+  loginUser,
+  getUserProfile,
+  updateUserProfile,
+} = require("../controllers/authController");
 const { protect } = require("../middlewares/authMiddleware");
 const router = express.Router();
 const upload = require("../middlewares/uploadMiddleware");
@@ -10,15 +15,20 @@ router.post("/login", loginUser); // Login User
 router.get("/profile", protect, getUserProfile); // Get User Profile
 router.put("/profile", protect, updateUserProfile); // Update Profile
 
-
+// ✅ Upload Image Route (Cloudinary fix)
 router.post("/upload-image", upload.single("image"), (req, res) => {
   if (!req.file) {
     return res.status(400).json({ message: "No file uploaded" });
   }
-  const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`;
-  return res.status(200).json({ imageUrl });
+
+  // ⚡️ Direct Cloudinary ka URL return hoga
+  const imageUrl = req.file.path;
+
+  return res.status(200).json({
+    success: true,
+    message: "Image uploaded successfully",
+    imageUrl,
+  });
 });
-
-
 
 module.exports = router;
